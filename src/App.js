5984883192e6b63f4908058573713d16b8dc007c
waiscodes <b.wais@outlook.com>
2021-04-28 22:14:35 -0600

    created and imported signin component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Signin from "./components/Signin";
 
 function App() {
   return (
     <div className='App'>
       <h1>Hello World</h1>
+      <Signin />
     </div>
   );
 }
---
+++ src/components/Signin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signin = () => {
+  return (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+};
+
+export default Signin;
