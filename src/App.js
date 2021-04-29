bfbe90b5a1949de343941386940eb45ec7ddab59
waiscodes <b.wais@outlook.com>
2021-04-28 23:21:15 -0600

    imported bootstrap and added it to project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Signin from "./components/Signin";
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
       <Signin />
     </div>
   );
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { auth, twitterProvider } from "../fire";
+import { Button } from "react-bootstrap";
 
 const Signin = () => {
   return (
     <>
       <h1>Login</h1>
+      <Button>Sign In With Twitter</Button>
     </>
   );
 };
