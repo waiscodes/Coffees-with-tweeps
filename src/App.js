f9016e4a3cdf596db0f5fbb5404433a634cb5bfa
waiscodes <b.wais@outlook.com>
2021-04-28 23:46:43 -0600

    added a topics component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Signin from "./components/Signin";
+import Topics from "./components/Topics";
 
 function App() {
   return (
     <div className='App'>
       <Signin />
+      <Topics />
     </div>
   );
 }
---
+++ src/components/Topics.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const Topics = () => {
+  return (
+    <Card>
+      <Card.Body>
+        <h1>Topics</h1>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Topics;
