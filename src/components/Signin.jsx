06a2e7578085f246e584341ac2fd5f95e32ec265
waiscodes <b.wais@outlook.com>
2021-04-28 23:25:55 -0600

    Added functionality to sign in to twitter

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -3,10 +3,21 @@ import { auth, twitterProvider } from "../fire";
 import { Button } from "react-bootstrap";
 
 const Signin = () => {
+  const signInWithTwitter = () => {
+    auth
+      .signInWithPopup(twitterProvider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log("yikes");
+      });
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <Button>Sign In With Twitter</Button>
+      <Button onClick={signInWithTwitter}>Sign In With Twitter</Button>
     </>
   );
 };
